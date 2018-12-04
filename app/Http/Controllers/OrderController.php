d26ce71ae6139452a719b734cd966bf0488fa9e3
Vinod Agarwal <vinod.agarwal@nagarro.com>
2018-12-05 00:40:26 +0530

    added api and integration test cases

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Models\Distance;
+use App\Http\Models\Order;
+use App\Http\Requests\OrderIndexRequest;
+use App\Http\Requests\OrderUpdateRequest;
+use App\Http\Response\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Mockery\Exception;
+use Validator;
+
+class OrderController extends Controller
+{
+    /**
+     * @var \App\Http\Services\Order
+     */
+    protected $orderService;
+
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @param \App\Http\Services\Order $orderService
+     * @param Response                 $response
+     */
+    public function __construct(
+        \App\Http\Services\Order $orderService,
+        Response $response
+    ) {
+        $this->orderService = $orderService;
+        $this->response = $response;
+    }
+
+    /**
+     * Places a new order
+     *
+     * @param Request $request
+     *
+     * @return bool|\Illuminate\Http\JsonResponse|int|string
+     */
+    public function create(Request $request)
+    {
+        $distanceModel = new Distance();
+        $validator = Validator::make($request->all(), $distanceModel->rules());
+
+        try {
+            if ($validator->fails()) {
+                return $this->response->setError('INVALID_PARAMETERS', JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            if ($model = $this->orderService->createOrder($request)) {
+                $formattedResponse = $this->response->formatOrderAsResponse($model);
+
+                return $this->response->setSuccessResponse($formattedResponse);
+            } else {
+                $messages = $this->orderService->error;
+                $errorCode = $this->orderService->errorCode;
+
+                return $this->response->setError($messages, $errorCode);
+            }
+
+        } catch (Exception $e) {
+            return $this->response->sendError($e->getMessage(), JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Updates an order
+     *
+     * @param OrderUpdateRequest $request
+     * @param int                $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(OrderUpdateRequest $request, $id)
+    {
+        try {
+            $order = Order::findOrFail($id);
+
+            if ($order->status !== Order::UNASSIGNED_ORDER_STATUS) {
+                return $this->response->setError('Order already Taken', JsonResponse::HTTP_CONFLICT);
+            }
+
+            $order->exists = true;
+            $order->id = $id;
+            $order->status = Order::ASSIGNED_ORDER_STATUS;
+            $order->save();
+
+            return $this->response->setSuccess('SUCCESS', JsonResponse::HTTP_OK);
+        } catch (\Exception $e) {
+            return $this->response->setError('Invalid Order Id', JsonResponse::HTTP_EXPECTATION_FAILED);
+        }
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return JsonResponse
+     */
+    public function index(OrderIndexRequest $request)
+    {
+        try {
+            $page = (int) $request->get('page', 1);
+            $limit = (int) $request->get('limit', 1);
+
+            $records = $this->orderService->getList($page, $limit);
+
+            if (!empty($records)) {
+                $orders = [];
+
+                foreach ($records as $record) {
+                    $orders[] = $this->response->formatOrderAsResponse($record);
+                }
+
+                return $this->response->setSuccessResponse($orders);
+            } else {
+                return $this->response->setError('No Content Found', JsonResponse::HTTP_NO_CONTENT);
+            }
+        } catch (Exception $exception) {
+            return $this->response->setError($exception->getMessage(), JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ database/migrations/2018_12_04_112726_create_distance_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distance', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('initial_latitude', 10, 7);
+            $table->decimal('initial_longitude', 10, 7);
+            $table->decimal('final_latitude', 10, 7);
+            $table->decimal('final_longitude', 10, 7);
+
+            // $table->char('initial_latitude', 25);
+            // $table->char('initial_longitude', 25);
+            // $table->char('final_latitude', 25);
+            // $table->char('final_longitude', 25);
+            $table->integer('distance'); //Distance in meters
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distance');
+    }
+}
