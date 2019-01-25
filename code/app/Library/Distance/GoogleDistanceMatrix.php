54b6b790af0ca366307cdf1829602ca7744cbf5a
Vinod Agarwal <vinod.agarwal@nagarro.com>
2019-01-25 15:07:17 +0530

    Avoid repeating complex variable chaining

--- code/app/Library/Distance/GoogleDistanceMatrix.php
+++ code/app/Library/Distance/GoogleDistanceMatrix.php
@@ -18,13 +18,15 @@ class GoogleDistanceMatrix implements DistanceMatrixInterface
 
             $data = json_decode($data);
 
+            $dataElements = $data->rows[0]->elements[0];
+
             if ( !$data || $data->status == 'REQUEST_DENIED' || $data->status == 'OVER_QUERY_LIMIT' || $data->status == 'NOT_FOUND' || $data->status == 'ZERO_RESULTS' ) {
                 return (isset($data->status)) ? $data->status : 'GOOGLE_API_NULL_RESPONSE';
             }
 
-            return (int) $data->rows[0]->elements[0]->distance->value;
+            return (int) $dataElements->distance->value;
         } catch (\Exception $e) {
-            return (isset($data->rows[0]->elements[0]->status)) ? $data->rows[0]->elements[0]->status : 'GOOGLE_API_NULL_RESPONSE';
+            return (isset($dataElements->status)) ? $dataElements->status : 'GOOGLE_API_NULL_RESPONSE';
         }
     }
 }
